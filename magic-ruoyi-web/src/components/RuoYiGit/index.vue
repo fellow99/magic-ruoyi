3783d8e379a73e12aa56512fab54db9617a63a25
fellow99 <fellow99@163.com>
2026-04-29 12:05:40 +0800

    feat: magic-ruoyi-web主前端

---
+++ magic-ruoyi-web/src/components/RuoYiGit/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script setup lang="ts">
+function goto() {
+  window.open(url.value);
+}
+</script>
---
+++ magic-ruoyi-web/src/views/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-container home">
+  </div>
+</template>
+
+<script setup name="Index" lang="ts">
+const goTarget = (url: string) => {
+  window.open(url, '__blank');
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+}
+</style>
